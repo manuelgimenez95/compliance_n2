7c536eac20a644e5a019d6e61eb77c48980ac94a
Manuel Gimenez <manuelgimenez95@gmail.com>
2026-02-20 11:58:39 +0000

    Actualización completa del proyecto desde ZIP

--- app.py
+++ app.py
@@ -1,14 +1,8 @@
 import streamlit as st
 from engine import process_files, generate_final_csv, FINALIDAD_MAP
-import gspread
-import json
-import os
-from oauth2client.service_account import ServiceAccountCredentials
-from datetime import datetime
-
 
 st.set_page_config(layout="wide")
-st.title("Unifica todas tus reservas en formato válido para N2")
+st.title("Generador oficial CSV NRUA")
 
 nrua_input = st.text_input("Introduce NRUA (si varios separados por coma)")
 year_target = st.number_input("Año a generar", min_value=2000, max_value=2100, value=2025)
@@ -26,9 +20,9 @@ finalidad_mode = st.radio(
 
 finalidad_global = None
 
-if finalidad_mode == "¿Quieres asignar una misma finalidad a todas las reservas? (Puedes editar posteriormente).":
+if finalidad_mode == "Asignar una finalidad a todas":
     finalidad_global = st.selectbox(
-        "Selecciona la finalidad por defecto",
+        "Selecciona finalidad",
         list(FINALIDAD_MAP.keys()),
         format_func=lambda x: f"{x} - {FINALIDAD_MAP[x]}"
     )
@@ -81,57 +75,3 @@ if uploaded_files and nrua_input:
                     file_name="reservas_nrua.csv",
                     mime="text/csv"
                 )
-# -------- FUNCIÓN PARA GUARDAR EN GOOGLE SHEETS --------
-
-def save_lead(email, tipo_analisis):
-    scope = [
-        "https://spreadsheets.google.com/feeds",
-        "https://www.googleapis.com/auth/drive",
-    ]
-
-    credentials_dict = json.loads(os.environ["GOOGLE_CREDENTIALS"])
-    credentials = ServiceAccountCredentials.from_json_keyfile_dict(
-        credentials_dict, scope
-    )
-
-    client = gspread.authorize(credentials)
-    sheet = client.open("Leads DepositoN2").sheet1
-
-    sheet.append_row([
-        datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-        email,
-        tipo_analisis
-    ])
-
-# -------- INTERFAZ --------
-
-st.subheader("Vista preliminar completada")
-
-st.info("Para descargar el informe completo necesitamos tus datos.")
-
-email = st.text_input("Correo electrónico")
-consent = st.checkbox("Acepto la política de privacidad")
-
-if st.button("Descargar informe completo"):
-    if email and consent:
-        save_lead(email, "Deposito N2")
-
-        st.success("Informe listo para descarga")
-
-        with open("informe_generado.pdf", "rb") as f:
-            st.download_button(
-                label="Descargar PDF",
-                data=f,
-                file_name="informe_deposito_n2.pdf",
-                mime="application/pdf"
-            )
-    else:
-        st.error("Debes completar el formulario y aceptar la política.")
-
-
-
-
-
-
-
-
